46f096f43a5bb3f85a968cb050d177cfb4e09c0a
MaxEMyth <maxmele@hotmail.com>
2024-09-04 11:26:41 -0300

    Solución de Ej 2 usando comprensiones.

--- Haskell/guia_4.hs
+++ Haskell/guia_4.hs
@@ -8,3 +8,17 @@ fibonacci 1 = 1
 fibonacci n = fibonacci (n-1) + fibonacci(n-2)
 
 -- * Ejercicio 2
+parteEntera :: Float -> Integer
+{-
+  problema parteEntera (x: R) : Z {
+    requiere: {True }
+    asegura: { resultado ≤ x < resultado + 1 }
+    }
+-}
+{- Solución con Comprensión
+parteEntera x = head [i | i <- l x, (fromInteger i)<=x, x<(fromInteger (i+1))]
+ where 
+  l x
+   | x>=0 = [0, 1..]
+   | x < 0 = [0, -1..]
+-}
